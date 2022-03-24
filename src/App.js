034c63445c0753f716ceae7876ae7f79a30da4cd
Grept <tomjansen.408@gmail.com>
2022-03-24 17:01:22 +0100

    2. Made a start with part 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Switch, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <></>
-  );
+    return (
+        <>
+            <Switch>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+            </Switch>
+
+        </>
+    );
 }
 
 export default App;
